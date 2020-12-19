8881bd7ec70b960d531cc27a46f05704d5690814
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2020-12-19 14:27:19 +0100

    build eslint rules

--- config/linting/custom-eslint-rules/built-rules/rule-helpers/check-test-imports.js
+++ config/linting/custom-eslint-rules/built-rules/rule-helpers/check-test-imports.js
@@ -1,37 +1,35 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /* eslint-disable @typescript-eslint/camelcase */
 const errorMessages = {
-  __testImport__wrongImportFromTestFile:
-    'Test files are only allowed to relatively import their local helper files, and the file they are testing (which should have the same basename as the test). Anything else should either not need to be imported or be placed in a helper file',
-  __testImport__wrongImportFromTestHelper:
-    'Test helper files are only allowed to import third party dependencies',
+    __testImport__wrongImportFromTestFile: 'Test files are only allowed to relatively import their local helper files, and the file they are testing (which should have the same basename as the test). Anything else should either not need to be imported or be placed in a helper file',
+    __testImport__wrongImportFromTestHelper: 'Test helper files are only allowed to import third party dependencies',
 };
 function getTestImportErrorMessages() {
-  return Object.assign({}, errorMessages);
+    return Object.assign({}, errorMessages);
 }
 exports.getTestImportErrorMessages = getTestImportErrorMessages;
-function checkTestImports({ currentPath, importPath }) {
-  if (currentPath.isInternalTestFile()) {
-    return checkTestFile();
-  } else if (currentPath.isTestHelperFile()) {
-    return checkTestHelper();
-  }
-  return null;
-  function checkTestFile() {
-    const isAllowed =
-      importPath.isTestHelper() ||
-      importPath.isSourceFileForTestPath(currentPath);
-    if (!isAllowed) {
-      return '__testImport__wrongImportFromTestFile';
+function checkTestImports({ currentPath, importPath, }) {
+    if (currentPath.isInternalTestFile()) {
+        return checkTestFile();
+    }
+    else if (currentPath.isTestHelperFile()) {
+        return checkTestHelper();
     }
     return null;
-  }
-  function checkTestHelper() {
-    if (importPath.isNotThirdParty()) {
-      return '__testImport__wrongImportFromTestHelper';
+    function checkTestFile() {
+        const isAllowed = importPath.isTestHelper() ||
+            importPath.isSourceFileForTestPath(currentPath);
+        if (!isAllowed) {
+            return '__testImport__wrongImportFromTestFile';
+        }
+        return null;
     }
-    return null;
-  }
+    function checkTestHelper() {
+        if (importPath.isNotThirdParty()) {
+            return '__testImport__wrongImportFromTestHelper';
+        }
+        return null;
+    }
 }
 exports.checkTestImports = checkTestImports;
--- config/linting/custom-eslint-rules/built-rules/rule-helpers/path-parser.js
+++ config/linting/custom-eslint-rules/built-rules/rule-helpers/path-parser.js
@@ -1,58 +1,57 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const lodash_1 = require('lodash');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const lodash_1 = require("lodash");
 class PathParser {
-  constructor(path) {
-    this.parts = path.split('/');
-  }
-  fileName() {
-    return this.parts[this.length() - 1];
-  }
-  fileNameBase() {
-    return this.fileName().split('.')[0];
-  }
-  start() {
-    return this.parts[0];
-  }
-  length() {
-    return this.parts.length;
-  }
-  hasAncestor(expectedAncestor) {
-    return this.parts.includes(expectedAncestor);
-  }
-  directAncestorsAre(expectedAncestors, ...rest) {
-    expectedAncestors = diverseArgumentsToArray(expectedAncestors, rest);
-    const actualAncestors = this.getXAncestors(expectedAncestors.length);
-    return lodash_1.isEqual(expectedAncestors, actualAncestors);
-  }
-  allAncestorsAre(expectedAncestors, ...rest) {
-    expectedAncestors = diverseArgumentsToArray(expectedAncestors, rest);
-    const ancestorLength = this.length() - 1; // pathLength includes filename
-    const expectedNumberAncestorsCorrect =
-      ancestorLength === expectedAncestors.length;
-    if (expectedNumberAncestorsCorrect) {
-      return this.directAncestorsAre(expectedAncestors);
+    constructor(path) {
+        this.parts = path.split('/');
     }
-    return false;
-  }
-  getXAncestors(numAncestors) {
-    const lengthWithFilename = numAncestors + 1;
-    const pathParts = this.getLastPartsOfPath(lengthWithFilename);
-    const partsWithoutFilename = pathParts.slice(0, -1);
-    return partsWithoutFilename;
-  }
-  getLastPartsOfPath(numPartsOfPath) {
-    return this.parts.slice(-1 * numPartsOfPath);
-  }
+    fileName() {
+        return this.parts[this.length() - 1];
+    }
+    fileNameBase() {
+        return this.fileName().split('.')[0];
+    }
+    start() {
+        return this.parts[0];
+    }
+    length() {
+        return this.parts.length;
+    }
+    hasAncestor(expectedAncestor) {
+        return this.parts.includes(expectedAncestor);
+    }
+    directAncestorsAre(expectedAncestors, ...rest) {
+        expectedAncestors = diverseArgumentsToArray(expectedAncestors, rest);
+        const actualAncestors = this.getXAncestors(expectedAncestors.length);
+        return lodash_1.isEqual(expectedAncestors, actualAncestors);
+    }
+    allAncestorsAre(expectedAncestors, ...rest) {
+        expectedAncestors = diverseArgumentsToArray(expectedAncestors, rest);
+        const ancestorLength = this.length() - 1; // pathLength includes filename
+        const expectedNumberAncestorsCorrect = ancestorLength === expectedAncestors.length;
+        if (expectedNumberAncestorsCorrect) {
+            return this.directAncestorsAre(expectedAncestors);
+        }
+        return false;
+    }
+    getXAncestors(numAncestors) {
+        const lengthWithFilename = numAncestors + 1;
+        const pathParts = this.getLastPartsOfPath(lengthWithFilename);
+        const partsWithoutFilename = pathParts.slice(0, -1);
+        return partsWithoutFilename;
+    }
+    getLastPartsOfPath(numPartsOfPath) {
+        return this.parts.slice(-1 * numPartsOfPath);
+    }
 }
 exports.PathParser = PathParser;
 function diverseArgumentsToArray(expectedParents, rest) {
-  const allExpectedParents = [];
-  if (typeof expectedParents === 'string')
-    allExpectedParents.push(expectedParents);
-  else {
-    allExpectedParents.push(...expectedParents);
-  }
-  allExpectedParents.push(...rest);
-  return allExpectedParents;
+    const allExpectedParents = [];
+    if (typeof expectedParents === 'string')
+        allExpectedParents.push(expectedParents);
+    else {
+        allExpectedParents.push(...expectedParents);
+    }
+    allExpectedParents.push(...rest);
+    return allExpectedParents;
 }
